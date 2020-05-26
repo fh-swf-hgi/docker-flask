0da9fa9c66e0d46b3560552d55fdd4773e6bc09c
Heiner Giefers <hgiefers@users.noreply.github.com>
2020-05-26 02:27:44 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hallo %s!" % name
+
+@app.route('/umdrehen/<wort>')
+def umderehen(wort):
+    s = wort[::-1]
+    return "%s" % s
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', debug=True)
